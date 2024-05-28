e5c7c60ea732c7fbea6a4940818aec1b472cf0e6
Diego Alves <revolution22@live.com>
2024-05-28 17:30:08 -0300

    Add users, router, userHandlers and middleware

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,13 +5,16 @@ import (
 	"github.com/DieegoAlves/API/internal/entity"
 	"github.com/DieegoAlves/API/internal/infra/database"
 	"github.com/DieegoAlves/API/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"net/http"
 )
 
 func main() {
-	_, err := configs.LoadConfig("./cmd/server")
+	configs, err := configs.LoadConfig("./cmd/server")
 	if err != nil {
 		panic(err)
 	}
@@ -20,10 +23,40 @@ func main() {
 		panic(err)
 	}
 	db.AutoMigrate(&entity.User{}, &entity.Product{})
+
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
-	http.HandleFunc("/products", productHandler.CreateProduct)
-	http.ListenAndServe(":8000", nil)
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB, configs.TokenAuth, configs.JWTExpiresIn)
 
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	//Middleware criado na mão
+	//r.Use(LogRequest)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Get("/", productHandler.GetAllProducts)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Post("/users", userHandler.CreateUser)
+	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	http.ListenAndServe(":8000", r)
 }
+
+//Middleware feita na mão
+//func LogRequest(next http.Handler) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		log.Printf("Request: %s %s", r.Method, r.URL.Path)
+//		next.ServeHTTP(w, r)
+//	})
+//}
